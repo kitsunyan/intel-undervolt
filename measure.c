428f86d013a2f7c1aca968c2a33a80736da68a74
kitsunyan <kitsunyan@airmail.cc>
2019-01-19 05:43:17 +0300

    Report temperatures and frequency as well

--- measure.c
+++ measure.c
@@ -1,7 +1,11 @@
 #include "measure.h"
+#include "util.h"
 
 #include <dirent.h>
 #include <fcntl.h>
+#include <iconv.h>
+#include <langinfo.h>
+#include <locale.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -11,7 +15,8 @@
 #include <time.h>
 #include <unistd.h>
 
-#define POWERCAP "/sys/class/powercap"
+#define DIR_POWERCAP "/sys/class/powercap"
+#define DIR_HWMON "/sys/class/hwmon"
 #define BUFSZ 80
 
 typedef struct {
@@ -20,18 +25,37 @@ typedef struct {
 	char * dir;
 	int64_t last;
 	struct timespec time;
-} rapl_list_t;
+} powercap_list_t;
 
-static void print_next(rapl_list_t * ilst, int maxname, char * buf) {
-	if (!ilst) {
-		/* clear the screen */
-		printf("\033[H\033[J");
-		fflush(0);
+typedef struct {
+	void * next;
+	char * name;
+	char * dir;
+	int index;
+} hwmon_list_t;
+
+static void write_maxname(const char * name, int maxname) {
+	int len = strlen(name);
+	write(0, name, len);
+	write(0, ":", 1);
+	int i;
+	for (i = len - 1; i < maxname; i++) {
+		write(0, " ", 1);
+	}
+}
+
+static void print_powercap_next(powercap_list_t * lst, int maxname, char * buf,
+	bool * nl, bool * nll) {
+	if (!lst) {
 		return;
 	}
-	print_next(ilst->next, maxname, buf);
+	bool nlla = false;
+	if (!nll) {
+		nll = &nlla;
+	}
+	print_powercap_next(lst->next, maxname, buf, nl, nll);
 
-	sprintf(buf, POWERCAP "/%s/energy_uj", ilst->dir);
+	sprintf(buf, DIR_POWERCAP "/%s/energy_uj", lst->dir);
 	int fd = open(buf, O_RDONLY);
 	if (fd >= 0) {
 		int count = read(fd, buf, BUFSZ - 1);
@@ -40,31 +64,203 @@ static void print_next(rapl_list_t * ilst, int maxname, char * buf) {
 			int64_t val = (int64_t) atoll(buf);
 			struct timespec tnow;
 			clock_gettime(CLOCK_MONOTONIC, &tnow);
-			if (ilst->last > 0) {
+			double dval = 0;
+			if (lst->last > 0) {
 				struct timespec tdiff;
-				tdiff.tv_sec = tnow.tv_sec - ilst->time.tv_sec;
-				tdiff.tv_nsec = tnow.tv_nsec - ilst->time.tv_nsec;
+				tdiff.tv_sec = tnow.tv_sec - lst->time.tv_sec;
+				tdiff.tv_nsec = tnow.tv_nsec - lst->time.tv_nsec;
 				while (tdiff.tv_nsec < 0) {
 					tdiff.tv_nsec += 1000000000;
 					tdiff.tv_sec--;
 				}
 				int64_t diff = (int64_t) (tdiff.tv_sec * 1000000000 +
 					tdiff.tv_nsec);
-				double dval = (double) (val - ilst->last) * 1000 / diff;
-				int len = strlen(ilst->name);
-				write(0, ilst->name, len);
-				write(0, ":", 1);
-				int i;
-				for (i = len - 1; i < maxname; i++) {
-					write(0, " ", 1);
-				}
-				printf("%7.03f W\n", ilst->name, dval);
+				dval = (double) (val - lst->last) * 1000 / diff;
 			}
-			ilst->last = val;
-			ilst->time = tnow;
+			NEW_LINE(nl, *nll);
+			write_maxname(lst->name, maxname);
+			printf("%9.03f W\n", lst->name, dval);
+			lst->last = val;
+			lst->time = tnow;
 		}
 		close(fd);
 	}
+}
+
+static void print_hwmon_next(hwmon_list_t * lst, int maxname, char * buf,
+	char * degstr, bool * nl, bool * nll) {
+	if (!lst) {
+		return;
+	}
+	bool nlla = false;
+	if (!nll) {
+		nll = &nlla;
+	}
+	print_hwmon_next(lst->next, maxname, buf, degstr, nl, nll);
+
+	sprintf(buf, DIR_HWMON "/%s/temp%d_input", lst->dir, lst->index);
+	int fd = open(buf, O_RDONLY);
+	if (fd >= 0) {
+		int count = read(fd, buf, BUFSZ - 1);
+		if (count > 0) {
+			buf[buf[count - 1] == '\n' ? count - 1 : count] = '\0';
+			double dval = atol(buf) / 1000.;
+			int len = strlen(lst->name);
+			NEW_LINE(nl, *nll);
+			write_maxname(lst->name, maxname);
+			printf("%9.03f%s\n", dval, degstr);
+		}
+		close(fd);
+	}
+}
+
+static void print_cpufreq(int maxname, char * buf, bool * nl) {
+	bool nll = false;
+
+	int i;
+	for (i = 0;; i++) {
+		sprintf(buf, "/sys/bus/cpu/devices/cpu%d/cpufreq/scaling_cur_freq", i);
+		int fd = open(buf, O_RDONLY);
+		if (fd < 0) {
+			break;
+		}
+		int count = read(fd, buf, BUFSZ - 1);
+		if (count > 0) {
+			buf[buf[count - 1] == '\n' ? count - 1 : count] = '\0';
+			double dval = atol(buf) / 1000.;
+			sprintf(buf, "Core %d", i);
+			NEW_LINE(nl, nll);
+			write_maxname(buf, maxname);
+			printf("%9.03f MHz\n", dval);
+		}
+		close(fd);
+	}
+}
+
+static powercap_list_t * get_powercap(int * maxname) {
+	char buf[BUFSZ];
+	powercap_list_t * lst = NULL;
+
+	DIR * dir = opendir(DIR_POWERCAP);
+	if (dir == NULL) {
+		fprintf(stderr, "Failed to open powercap directory\n");
+		return NULL;
+	}
+	struct dirent * dirent;
+	while (dirent = readdir(dir)) {
+		if (strstr(dirent->d_name, ":") && strlen(dirent->d_name) <= 30) {
+			sprintf(buf, DIR_POWERCAP "/%s/name", dirent->d_name);
+			int fd = open(buf, O_RDONLY);
+			if (fd >= 0) {
+				int count = read(fd, buf, BUFSZ - 1);
+				if (count > 1) {
+					int nlen = buf[count - 1] == '\n' ? count - 2 : count - 1;
+					buf[nlen + 1] = '\0';
+					powercap_list_t * nlst = malloc(sizeof(powercap_list_t));
+					nlst->next = lst;
+					lst = nlst;
+					nlst->name = malloc(nlen + 1);
+					memcpy(nlst->name, buf, nlen + 1);
+					int dlen = strlen(dirent->d_name);
+					nlst->dir = malloc(dlen + 1);
+					memcpy(nlst->dir, dirent->d_name, dlen + 1);
+					nlst->last = 0;
+					if (maxname) {
+						*maxname = nlen > *maxname ? nlen : *maxname;
+					}
+				}
+				close(fd);
+			}
+		}
+	}
+	closedir(dir);
+
+	return lst;
+}
+
+static bool get_hwmon(const char * name, char * out) {
+	char buf[BUFSZ];
+
+	DIR * dir = opendir(DIR_HWMON);
+	if (dir == NULL) {
+		fprintf(stderr, "Failed to open hwmon directory\n");
+		return NULL;
+	}
+	struct dirent * dirent;
+	while (dirent = readdir(dir)) {
+		if (strlen(dirent->d_name) <= 30) {
+			sprintf(buf, DIR_HWMON "/%s/name", dirent->d_name);
+			int fd = open(buf, O_RDONLY);
+			if (fd >= 0) {
+				int count = read(fd, buf, BUFSZ - 1);
+				if (count > 1) {
+					int nlen = buf[count - 1] == '\n' ? count - 2 : count - 1;
+					buf[nlen + 1] = '\0';
+					if (!strcmp(buf, name)) {
+						strcpy(out, dirent->d_name);
+						close(fd);
+						closedir(dir);
+						return true;
+					}
+				}
+				close(fd);
+			}
+		}
+	}
+	closedir(dir);
+
+	return false;
+}
+
+static hwmon_list_t * get_coretemp(int * maxname) {
+	char hdir[BUFSZ];
+	char buf[BUFSZ];
+	hwmon_list_t * lst = NULL;
+
+	if (!get_hwmon("coretemp", hdir)) {
+		fprintf(stderr, "Failed to find coretemp hwmon\n");
+		return NULL;
+	}
+
+	int i;
+	for (i = 1;; i++) {
+		sprintf(buf, DIR_HWMON "/%s/temp%d_input", hdir, i);
+		int fd = open(buf, O_RDONLY);
+		if (fd < 0) {
+			break;
+		}
+		sprintf(buf, DIR_HWMON "/%s/temp%d_label", hdir, i);
+		fd = open(buf, O_RDONLY);
+		char * name = NULL;
+		if (fd >= 0) {
+			int count = read(fd, buf, BUFSZ - 1);
+			if (count > 1) {
+				int nlen = buf[count - 1] == '\n' ? count - 2 : count - 1;
+				buf[nlen + 1] = '\0';
+				name = malloc(strlen(buf) + 1);
+				strcpy(name, buf);
+			}
+			close(fd);
+		}
+		if (!name) {
+			sprintf(buf, "temp%d", i);
+			name = malloc(strlen(buf) + 1);
+			strcpy(name, buf);
+		}
+		hwmon_list_t * nlst = malloc(sizeof(hwmon_list_t));
+		nlst->next = lst;
+		lst = nlst;
+		nlst->name = name;
+		nlst->dir = malloc(strlen(hdir) + 1);
+		strcpy(nlst->dir, hdir);
+		nlst->index = i;
+		if (maxname) {
+			int len = strlen(name);
+			*maxname = len > *maxname ? len : *maxname;
+		}
+	}
+
+	return lst;
 }
 
 static bool interrupted;
@@ -75,40 +271,22 @@ static void sigint_handler(int sig) {
 
 int measure_mode() {
 	char buf[BUFSZ];
-	rapl_list_t * glst = NULL;
 	int maxname = 0;
+	powercap_list_t * powercap_list = get_powercap(&maxname);
+	hwmon_list_t * coretemp_list = get_coretemp(&maxname);
+	char degstr[5] = " C";
 
-	DIR * dir = opendir(POWERCAP);
-	if (dir == NULL) {
-		fprintf(stderr, "Failed to open powercap directory\n");
-		return 1;
+	setlocale(LC_CTYPE, "");
+	iconv_t ic = iconv_open(nl_langinfo(CODESET), "ISO-8859-1");
+	if (ic != (iconv_t) -1) {
+		char in[3] = "\260C";
+		char * inptr = in;
+		char * outptr = degstr;
+		size_t insize = 3;
+		size_t outsize = sizeof(degstr);
+		int x = iconv(ic, &inptr, &insize, &outptr, &outsize);
+		iconv_close(ic);
 	}
-	struct dirent * dirent;
-	while (dirent = readdir(dir)) {
-		if (strstr(dirent->d_name, ":") && strlen(dirent->d_name) <= 30) {
-			sprintf(buf, POWERCAP "/%s/name", dirent->d_name);
-			int fd = open(buf, O_RDONLY);
-			if (fd >= 0) {
-				int count = read(fd, buf, BUFSZ - 1);
-				if (count > 1) {
-					int nlen = buf[count - 1] == '\n' ? count - 2 : count - 1;
-					buf[nlen + 1] = '\0';
-					rapl_list_t * nlst = malloc(sizeof(rapl_list_t));
-					nlst->next = glst;
-					glst = nlst;
-					nlst->name = malloc(nlen + 1);
-					memcpy(nlst->name, buf, nlen + 1);
-					int dlen = strlen(dirent->d_name);
-					nlst->dir = malloc(dlen + 1);
-					memcpy(nlst->dir, dirent->d_name, dlen + 1);
-					nlst->last = 0;
-					maxname = nlen > maxname ? nlen : maxname;
-				}
-				close(fd);
-			}
-		}
-	}
-	closedir(dir);
 
 	interrupted = false;
 	struct sigaction act;
@@ -116,17 +294,34 @@ int measure_mode() {
 	act.sa_handler = sigint_handler;
 	sigaction(SIGINT, &act, NULL);
 
+	/* clear the screen */
+	printf("\033[H\033[J");
 	while (!interrupted) {
-		print_next(glst, maxname, buf);
-		sleep(1);
+		/* move the cursor */
+		printf("\033[H");
+		fflush(0);
+		bool nl = false;
+		print_powercap_next(powercap_list, maxname, buf, &nl, NULL);
+		print_hwmon_next(coretemp_list, maxname, buf, degstr, &nl, NULL);
+		print_cpufreq(maxname, buf, &nl);
+		if (!interrupted) {
+			sleep(1);
+		}
 	}
 
-	while (glst) {
-		rapl_list_t * nlst = glst->next;
-		free(glst->name);
-		free(glst->dir);
-		free(glst);
-		glst = nlst;
+	while (powercap_list) {
+		powercap_list_t * next = powercap_list->next;
+		free(powercap_list->name);
+		free(powercap_list->dir);
+		free(powercap_list);
+		powercap_list = next;
+	}
+	while (coretemp_list) {
+		hwmon_list_t * next = coretemp_list->next;
+		free(coretemp_list->name);
+		free(coretemp_list->dir);
+		free(coretemp_list);
+		coretemp_list = next;
 	}
 
 	return 0;
